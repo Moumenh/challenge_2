aba24797ffaccace6438f2f026daeb96724b00e4
Moumenh <mohanbali94@gmail.com>
2020-11-03 11:47:37 +0200

    first initializations

--- server.js
+++ server.js
@@ -1,2 +1,26 @@
+const express = require('express')
 
+
+const app = express()
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+app.use(express.static(__dirname + '/./client'));
+
+
+app.post('/json' , (req,res) => {
+    console.log(req.body)
+    res.send('first')
+})
+
+app.get('/json' , (req,res) => {
+    // console.log(req.body)
+    res.send('csv sent')
+})
+
+
+app.listen(3000,() => {
+    console.log('server is listening ')
+})
 
